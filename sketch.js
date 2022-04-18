9a1134563b16451fb330ecda8ac5e72e9a8a6a3c
callllll <91920400+callllll@users.noreply.github.com>
2022-04-17 23:34:05 -0700

    update sketch

--- sketch.js
+++ sketch.js
@@ -98,10 +98,26 @@ function setup() {
 }
 
 function draw() {
+  switch (gameState) {
+    case 'title':
+      titleScreen();
+      break;
+  }
   background(255);
   click1.draw();
   click2.draw();
   click3.draw();
   click4.draw();
   click5.draw();
+}
+
+function titleScreen() {
+  background('black');
+  stroke(255);
+  fill(255);
+  textSize(100);
+  textAlign(CENTER);
+  text('Wolf', width*0.5, height*0.33);
+  textSize(35);
+  text('Press "W" To Start Game', width*0.5, height*0.66);
 }
